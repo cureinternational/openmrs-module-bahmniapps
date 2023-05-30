3058f70ca64a9a400958154812178164c610a544
Arjun G <91885483+Arjun-Go@users.noreply.github.com>
2023-05-30 13:04:39 +0530

    Arjun | BAH-3018 | Capture patient registration location (#580)

--- ui/app/registration/controllers/createPatientController.js
+++ ui/app/registration/controllers/createPatientController.js
@@ -37,6 +37,9 @@ angular.module('bahmni.registration')
                 var isConcept = function (personAttributeType) {
                     return personAttributeType.format === "org.openmrs.Concept";
                 };
+                var isLocation = function (personAttributeType) {
+                    return personAttributeType.format === "org.openmrs.Location";
+                };
 
                 var setDefaultAnswer = function (personAttributeType) {
                     $scope.patient[personAttributeType.name] = defaults[personAttributeType.name];
@@ -52,6 +55,19 @@ angular.module('bahmni.registration')
                         $scope.patient[personAttributeType.name] = {
                             conceptUuid: answer.conceptId,
                             value: answer.fullySpecifiedName
+                        };
+                    }).value();
+                };
+                var setDefaultLocation = function (personAttributeType) {
+                    var defaultAnswer = defaults[personAttributeType.name];
+                    var isDefaultAnswer = function (answer) {
+                        return answer.name === defaultAnswer;
+                    };
+
+                    _.chain(personAttributeType.answers).filter(isDefaultAnswer).each(function (answer) {
+                        $scope.patient[personAttributeType.name] = {
+                            uuid: answer.uuid,
+                            value: answer.name
                         };
                     }).value();
                 };
@@ -81,6 +97,7 @@ angular.module('bahmni.registration')
                     .each(setDefaultAnswer).value();
 
                 _.chain(defaultsWithAnswers).filter(isConcept).each(setDefaultConcept).value();
+                _.chain(defaultsWithAnswers).filter(isLocation).each(setDefaultLocation).value();
                 _.chain(defaultsWithAnswers).filter(isDateType).each(setDefaultValue).value();
                 if ($scope.relatedIdentifierAttribute && $scope.relatedIdentifierAttribute.name) {
                     $scope.patient[$scope.relatedIdentifierAttribute.name] = false;
