5b813312912d1a9b12b92c2f8270cde6e0442a63
Vikramdas Vaishnav <vikramdas.vaishnav@thoughtworks.com>
2024-12-17 13:20:47 +0530

    Update the to Navigation URL to be consisten

--- ui/app/common/displaycontrols/patientprofile/directives/patientProfile.js
+++ ui/app/common/displaycontrols/patientprofile/directives/patientProfile.js
@@ -38,9 +38,10 @@
                     $scope.isProviderRelationship = function (relationship) {
                         return _.includes($rootScope.relationshipTypeMap.provider, relationship.relationshipType.aIsToB);
                     };
+                    console.log("Demo Test : ", $stateParams.configName);
+                    $scope.configName = $stateParams.configName || Bahmni.Common.Constants.defaultExtensionName;
                     $scope.openPatientDashboard = function (patientUuid) {
-                        var configName = $stateParams.configName || Bahmni.Common.Constants.defaultExtensionName;
-                        $window.open("../clinical/#/" + configName + "/patient/" + patientUuid + "/dashboard");
+                        $window.open("../clinical/#/" + $scope.configName + "/patient/" + patientUuid + "/dashboard");
                     };
                     $scope.iconAttributeConfig = appService.getAppDescriptor().getConfigValue('iconAttribute') || {};
                     var assignPatientDetails = function () {
