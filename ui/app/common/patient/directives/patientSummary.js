02c0d1f235aef5b0f10c1f07543d2ef2d8621187
Pooja Sastry <pooja.sastry@thoughtworks.com>
2025-11-19 09:52:48 +0530

    Feature upgrade age in days months (#29)
    * Age in days months (#7)
    
    * A - 1209055939299991 |  Added Supprt for partient ADT page
    
    * Changes format in OT Scheduling List View
    
    * Resolving ESLint syntax waringing
    
    * Correction of Syntax as Per ESLint 6
    
    * Ward List changes
    
    * ESLint changes
    
    * Rever the wardListController changes
    
    * Fixing ESLint Error
    
    * Update validate_pr.yml
    
    Adding flag --force to test the build
    
    * Update validate_pr.yml
    
    Reverting the changes
    
    * A - 1209055939299991 |  Age Card Test case added (#10)
    
    * A - 1209055939299991 |  Age Card Test case added
    
    * Removed the Commnet
    
    ---------
    
    Co-authored-by: Vikramdas Vaishnmav <vikramdas.vaishnav@thoughtworks.com>

--- ui/app/common/patient/directives/patientSummary.js
+++ ui/app/common/patient/directives/patientSummary.js
@@ -1,28 +1,62 @@
 'use strict';
 
-angular.module('bahmni.common.patient').directive('patientSummary', function () {
-    var link = function ($scope) {
-        $scope.showPatientDetails = false;
-        $scope.togglePatientDetails = function () {
-            $scope.showPatientDetails = !$scope.showPatientDetails;
+angular.module('bahmni.common.patient')
+    .directive('patientSummary', ['$translate', '$sce', function ($translate, $sce) {
+        var link = function ($scope) {
+            $scope.showPatientDetails = false;
+            $scope.togglePatientDetails = function () {
+                $scope.showPatientDetails = !$scope.showPatientDetails;
+            };
+            $scope.onImageClick = function () {
+                if ($scope.onImageClickHandler) {
+                    $scope.onImageClickHandler();
+                }
+            };
+
+            $scope.calculateAge = function (birthDate) {
+                if (!birthDate) return "";
+
+                var DateUtil = Bahmni.Common.Util.DateUtil;
+                var age = DateUtil.diffInYearsMonthsDays(birthDate, DateUtil.now());
+
+                var ageInString = "";
+                if (age.years) {
+                    ageInString += age.years + " <span> " + $translate.instant("CLINICAL_YEARS_TRANSLATION_KEY") + " </span>";
+                }
+                if (age.months) {
+                    // Only include months when the value is greater than 0
+                    ageInString += " " + age.months + " <span> " + $translate.instant("CLINICAL_MONTHS_TRANSLATION_KEY") + " </span>";
+                } else if (age.years && age.days) {
+                    ageInString += " 0 <span> " + $translate.instant("CLINICAL_MONTHS_TRANSLATION_KEY") + " </span>";
+                }
+                if (age.days) {
+                    ageInString += " " + age.days + " <span> " + $translate.instant("CLINICAL_DAYS_TRANSLATION_KEY") + " </span>";
+                }
+
+                return ageInString.trim();
+            };
+
+            $scope.computeAgeDisplay = function () {
+                if (!$scope.patient || !$scope.patient.birthdate) return;
+                $scope.displayAge = $sce.trustAsHtml($scope.calculateAge($scope.patient.birthdate));
+            };
+
+            $scope.computeAgeDisplay();
+
+            $scope.$watch('patient', function () {
+                $scope.computeAgeDisplay();
+            }, true);
         };
 
-        $scope.onImageClick = function () {
-            if ($scope.onImageClickHandler) {
-                $scope.onImageClickHandler();
+        return {
+            restrict: 'E',
+            templateUrl: '../common/patient/header/views/patientSummary.html',
+            link: link,
+            required: 'patient',
+            scope: {
+                patient: "=",
+                bedDetails: "=",
+                onImageClickHandler: "&"
             }
         };
-    };
-
-    return {
-        restrict: 'E',
-        templateUrl: '../common/patient/header/views/patientSummary.html',
-        link: link,
-        required: 'patient',
-        scope: {
-            patient: "=",
-            bedDetails: "=",
-            onImageClickHandler: "&"
-        }
-    };
-});
+    }]);
