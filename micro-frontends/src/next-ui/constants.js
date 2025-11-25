8cd2c9747e4915284b4222467a4f680ac17c5aa3
Pooja Sastry <pooja.sastry@thoughtworks.com>
2025-11-25 16:04:10 +0530

    Pooja | Remove a repeated line of code in constants.js

--- micro-frontends/src/next-ui/constants.js
+++ micro-frontends/src/next-ui/constants.js
@@ -33,6 +33,5 @@ export const EMERGENCY_MEDICATIONS_BASE_URL = RESTWS_V1 + "/ipd/adhocMedicationA
 export const GET_PROVIDER_UUID_URL = RESTWS_V1 + "/session";
 export const SAVE_ALLERGIES_URL =  RESTWS_V1 + "/patient/{patientId}/allergy";
 export const NEXT_UI_CONFIG_PATH = hostUrl + "/bahmni_config/openmrs/";
-export const SAVE_ALLERGIES_URL =  RESTWS_V1 + "/patient/{patientId}/allergy";
 export const OT_NOTES_BASE_URL = RESTWS_V1 + "/notes";
 
