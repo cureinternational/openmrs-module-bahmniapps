cabc1b6fc1988854335f38929bfea240eeb1aa8b
Pooja Sastry <pooja.sastry@thoughtworks.com>
2025-11-26 12:34:07 +0530

    Pooja | feature-upgrade-translations-test-fixes | fixes for issue with translations and test cases

--- micro-frontends/src/next-ui/constants.js
+++ micro-frontends/src/next-ui/constants.js
@@ -13,7 +13,7 @@ const hostUrl = localStorage.getItem("host")
   : "";
 
 export const verifierFunction = "Verifier";
-
+export const document_images_path = "../../document_images/";
 const RESTWS_V1 = hostUrl + "/openmrs/ws/rest/v1";
 
 export const FORM_BASE_URL = RESTWS_V1 + "/bahmnicore/patient/{patientUuid}/forms";
@@ -33,6 +33,5 @@ export const EMERGENCY_MEDICATIONS_BASE_URL = RESTWS_V1 + "/ipd/adhocMedicationA
 export const GET_PROVIDER_UUID_URL = RESTWS_V1 + "/session";
 export const SAVE_ALLERGIES_URL =  RESTWS_V1 + "/patient/{patientId}/allergy";
 export const NEXT_UI_CONFIG_PATH = hostUrl + "/bahmni_config/openmrs/";
-export const SAVE_ALLERGIES_URL =  RESTWS_V1 + "/patient/{patientId}/allergy";
 export const OT_NOTES_BASE_URL = RESTWS_V1 + "/notes";
 
