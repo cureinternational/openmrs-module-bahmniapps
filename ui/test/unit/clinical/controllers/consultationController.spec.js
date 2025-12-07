9338484bd77199b8bb23e7c3283b3d8c8a9d593c
Vikramdas Vaishnav <vikramdas.vaishnav@thoughtworks.com>
2025-12-07 14:12:56 +0530

    Reverted the ConsultationController changes

--- ui/test/unit/clinical/controllers/consultationController.spec.js
+++ ui/test/unit/clinical/controllers/consultationController.spec.js
@@ -4,7 +4,7 @@ describe("ConsultationController", function () {
     var scope, rootScope, state, contextChangeHandler, urlHelper, location, clinicalAppConfigService,
         stateParams, appService, ngDialog, q, appDescriptor, controller, visitConfig, _window_, clinicalDashboardConfig,
         sessionService, conditionsService, encounterService, configurations, diagnosisService, messagingService, spinnerMock,
-        auditLogService, confirmBox, virtualConsultService, adhocTeleconsultationService, visitService;
+        auditLogService, confirmBox, virtualConsultService, adhocTeleconsultationService;
 
     var encounterData = {
         "bahmniDiagnoses": [],
@@ -146,8 +146,7 @@ describe("ConsultationController", function () {
             auditLogService: auditLogService,
             confirmBox: confirmBox,
             virtualConsultService: virtualConsultService,
-            adhocTeleconsultationService: adhocTeleconsultationService,
-            visitService: visitService
+            adhocTeleconsultationService: adhocTeleconsultationService
         });
     };
     var setUpServiceMocks = function () {
@@ -248,8 +247,6 @@ describe("ConsultationController", function () {
         diagnosisService = jasmine.createSpyObj('diagnosisService', ['populateDiagnosisInformation']);
         adhocTeleconsultationService = jasmine.createSpyObj('adhocTeleconsultationService', ['generateAdhocTeleconsultationLink']);
         virtualConsultService = jasmine.createSpyObj('virtualConsultService', ['launchMeeting']);
-        visitService = jasmine.createSpyObj('visitService', ['search']);
-        visitService.search.and.returnValue(specUtil.simplePromise({ data: { results: [] } }));
         encounterService.create.and.returnValue(specUtil.createFakePromise(encounterData));
         encounterService.create.and.callFake(function () {
             var deferred = Q.defer();
