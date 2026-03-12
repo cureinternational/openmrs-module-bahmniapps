57007ebd880ee7a014e0be1bcff6131482b640c9
Phanindra <v.tadikonda@thoughtworks.com>
2026-03-12 13:23:39 +0530

    Phani | HIVE-105171 | Update translations and Fix tests

--- ui/app/ot/controller/listViewController.js
+++ ui/app/ot/controller/listViewController.js
@@ -6,7 +6,7 @@ angular.module('bahmni.ot')
             var startDatetime = moment($scope.viewDate).toDate();
             var surgicalBlockMapper = new Bahmni.OT.SurgicalBlockMapper();
             var endDatetime = moment(startDatetime).endOf('day').toDate();
-            var listViewTranslations = appService.getAppDescriptor().getConfigValue("listViewTranslations") || {};
+            var listViewTranslations = appService.getAppDescriptor().getConfigValue("listViewTranslations") || [];
             $scope.defaultAttributeTranslations = surgicalAppointmentHelper.getDefaultAttributeTranslations(listViewTranslations);
             $scope.conceptFormatAttributeName = otUtils.getConceptFormatAttributeName();
             $scope.conceptFormatDropdownConstants = Bahmni.OT.Constants.notApplicableValues;
