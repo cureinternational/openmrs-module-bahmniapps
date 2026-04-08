891c5452a86289e3a7d825338c7071618f7f9e82
Phanindra-tw <v.tadikonda@thoughtworks.com>
2026-04-08 17:04:50 +0530

    Phani | HIVE-106250 | Fix issues in Form Comments and Approval (#112)

--- ui/app/common/displaycontrols/dashboard/directives/dashboard.js
+++ ui/app/common/displaycontrols/dashboard/directives/dashboard.js
@@ -36,7 +36,8 @@ angular.module('bahmni.common.displaycontrol.dashboard')
                     consultationMapper: new Bahmni.ConsultationMapper(configurations.dosageFrequencyConfig(), configurations.dosageInstructionConfig(),
                     configurations.consultationNoteConcept(), configurations.labOrderNotesConcept()),
                     editErrorMessage: $translate.instant('CLINICAL_FORM_ERRORS_MESSAGE_KEY'),
-                    showPrintOption: (dashboardConfig && dashboardConfig.printing) ? true : false
+                    showPrintOption: (dashboardConfig && dashboardConfig.printing) ? true : false,
+                    currentProvider: $rootScope.currentProvider
                 };
                 $scope.formApi = {
                     handleEditSave: function (encounter) {
