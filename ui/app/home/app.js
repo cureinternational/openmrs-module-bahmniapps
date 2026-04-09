fd242453b22996989574e8405cc94ad50555b1fa
SasikiranJ <sasikiran.jakkula@thoughtworks.com>
2026-04-09 13:08:34 +0530

    Add bahmni common config dep to the home app so that new directive will be applied

--- ui/app/home/app.js
+++ ui/app/home/app.js
@@ -2,7 +2,7 @@
 
 angular.module('bahmni.home', ['ui.router', 'httpErrorInterceptor', 'bahmni.common.domain', 'bahmni.common.i18n', 'bahmni.common.uiHelper', 'bahmni.common.util',
     'bahmni.common.appFramework', 'bahmni.common.logging', 'bahmni.common.routeErrorHandler', 'pascalprecht.translate', 'ngCookies',
-    'bahmni.common.models', 'bahmni.mfe.nextUi'])
+    'bahmni.common.models', 'bahmni.common.config', 'bahmni.mfe.nextUi'])
     .config(['$urlRouterProvider', '$stateProvider', '$httpProvider', '$bahmniTranslateProvider', '$compileProvider',
         function ($urlRouterProvider, $stateProvider, $httpProvider, $bahmniTranslateProvider, $compileProvider) {
             $urlRouterProvider.otherwise('/dashboard');
