0af14046e3af5f931a0871bd7446a2234aff558a
SasikiranJ <sasikiran.jakkula@thoughtworks.com>
2026-04-28 18:33:27 +0530

    Implement variable dosage modal

--- micro-frontends/src/next-ui/index.js
+++ micro-frontends/src/next-ui/index.js
@@ -4,6 +4,7 @@ import { FormDisplayControl } from "./Containers/formDisplayControl/FormDisplayC
 import { ProviderNotifications } from "./Containers/providerNotifications/ProviderNotifications";
 import { OtNotesSavePopup, OtNotesDeletePopup } from "./Containers/otNotes/OtNotes";
 import { OrdersDisplayControl } from "./Containers/OrdersDisplayControl/OrdersDisplayControl";
+import { VariableDoseProtocol } from "./Containers/variableDoseProtocol/VariableDoseProtocol";
 
 const MODULE_NAME = "bahmni.mfe.nextUi";
 
@@ -42,5 +43,10 @@ builder.createComponentWithTranslationForwarding(
 builder.createComponentWithTranslationForwarding(
     "OrdersDisplayControl",
     OrdersDisplayControl
+);
+
+builder.createComponentWithTranslationForwarding(
+    "VariableDoseProtocol",
+    VariableDoseProtocol
 );
 
