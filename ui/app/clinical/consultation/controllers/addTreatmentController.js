37b184b0303816d8cd852805d85e3081f9fa26ed
sasikiran-tw <sasikiran.jakkula@thoughtworks.com>
2026-04-30 09:57:03 +0530

    Implement variable dosage modal (#127)

--- ui/app/clinical/consultation/controllers/addTreatmentController.js
+++ ui/app/clinical/consultation/controllers/addTreatmentController.js
@@ -974,6 +974,37 @@ angular.module('bahmni.clinical')
                 showRulesInMedication(medicationConfig);
                 setRuleUnitsMap(medicationConfig);
                 setContinuousMedicationRoutes(medicationConfig);
+
+                $scope.showVariableDoseProtocol =
+                    appService.getAppDescriptor().getConfigValue('enableVariableDoseProtocol') === true;
+
+                if ($scope.showVariableDoseProtocol) {
+                    var orderSetConfig = medicationConfig && medicationConfig.tabConfig &&
+                        medicationConfig.tabConfig.allMedicationTabConfig &&
+                        medicationConfig.tabConfig.allMedicationTabConfig.orderSet;
+                    $scope.variableDoseHostData = {
+                        doseUnits: treatmentConfig.getDoseUnits(),
+                        routes: treatmentConfig.getRoutes(),
+                        dosingRules: treatmentConfig.dosingRules || [],
+                        drugFormDefaults: treatmentConfig.inputOptionsConfig.drugFormDefaults || {},
+                        dosageRuleUnitsMap: (orderSetConfig && orderSetConfig.dosageRuleUnitsMap) || {}
+                    };
+                    $scope.variableDoseHostApi = {
+                        onClose: function () {},
+                        onSave: function () {
+                            // prescribing handled in a future story
+                        },
+                        searchDrugs: function (term) {
+                            return new Promise(function (resolve) {
+                                $timeout(function () {
+                                    return drugService.search(term);
+                                }, 0).then(function (results) {
+                                    resolve(results || []);
+                                });
+                            });
+                        }
+                    };
+                }
             };
             init();
         }]);
