64f743ef778b25f28306d1972cb2cd4a937b3700
Pooja Sastry <pooja.sastry@thoughtworks.com>
2026-06-05 13:09:34 +0530

    Pooja | Hive-116894 | Templatized text and on hold status for future orders

--- micro-frontends/src/next-ui/constants.js
+++ micro-frontends/src/next-ui/constants.js
@@ -48,6 +48,7 @@ export const FHIR_TASK_STATUS_TO_UI_STATUS = {
     DRAFT: 'New',
     REQUESTED: 'Acknowledged',
     ACCEPTED: 'In Progress',
+    ONHOLD: 'On Hold',
     COMPLETED: 'Completed',
 };
 export const FORM_COMMENT = "FORM_COMMENT";
