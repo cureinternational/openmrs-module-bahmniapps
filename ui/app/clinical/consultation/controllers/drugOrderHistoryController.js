f46b6e9dc4665ac363f30201be637e2f285a298a
Lingeswaran Subramaniyam <lingeswaran.s@thoughtworks.com>
2026-06-30 17:36:33 +0530

    BAH-4834 Fix for dispensed all button got disabled with tick mark

--- ui/app/clinical/consultation/controllers/drugOrderHistoryController.js
+++ ui/app/clinical/consultation/controllers/drugOrderHistoryController.js
@@ -337,9 +337,6 @@ angular.module('bahmni.clinical')
             };
 
             $scope.shouldBeDisabled = function (drugOrder, orderAttribute) {
-                if ($scope.showPharmacistReviewBanner()) {
-                    return true;
-                }
                 if (drugOrder.isBeingEdited) {
                     return true;
                 }
