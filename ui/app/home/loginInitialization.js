bc0c5d2d2130cb93b546944f0e559fa3d5661262
Ramadevi Adepu <ramadeviadepu@Ramadevis-MacBook-Pro.local>
2026-08-12 19:27:57 +0530

    Update command palette authentication

--- ui/app/home/loginInitialization.js
+++ ui/app/home/loginInitialization.js
@@ -12,6 +12,8 @@ angular.module('bahmni.home')
                     function (response) {
                         deferrable.reject();
                         if (response.status) {
+                            // This block checks if status code is 401 and reloads the page instead of throwing a pop up error message
+                            // Refer BAH-2407 Clinical Module homepage is throwing error on Login Page issue.
                             if (response.status == 401) {
                                 location.reload();
                             } else {
